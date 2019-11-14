512319aa6eadb874c1e710e436f2100eda39b8ad
Justin Terry <Justin.Terry@student.csulb.edu>
2019-11-13 23:02:53 -0800

    Got through Updating the State

--- count-app/src/components/counter.jsx
+++ count-app/src/components/counter.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3'],
+        id: this.props.counter.id,
+        value: this.props.counter.value,
     }
 
     render() {
@@ -11,29 +11,30 @@ class Counter extends Component {
             <div>
                 <span className={this.getBadgeClasses()}>{ this.formatCount() }</span>
                 <button onClick={ this.handleIncrement } className="btn btn-success btn-sm m-2">+</button>
-                <button onClick={ this.handleDecrement } className="btn btn-danger btn-sm m-2">-</button>
+                <button onClick={ this.handleDecrement } className="btn btn-warning btn-sm m-2">-</button>
+                <button onClick={ () => this.props.onDelete(this.props.counter.id) } className="btn btn-danger btn-sm m-2">X</button>
             </div>
         );
     }
 
     // Using an arrow function here gives access to this because it inherits it
     handleIncrement = () => {
-        this.setState({ count: this.state.count+1 });
+        this.setState({ value: this.state.value+1 });
     }
 
     handleDecrement = () => {
-        this.setState({ count: this.state.count-1 });
+        this.setState({ value: this.state.value-1 });
     }
 
     getBadgeClasses() {
         var classes = "badge m-2 ";
-        classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+        classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+        const { value } = this.state;
+        return value === 0 ? 'Zero' : value;
     }
 }
 
--- count-app/src/components/counters.jsx
+++ count-app/src/components/counters.jsx
@@ -3,17 +3,26 @@ import Counter from './counter';
 
 class Counters extends Component {
     state = {
-
+        counters:[
+            { id: 1, value: 3 },
+            { id: 2, value: 2 },
+            { id: 3, value: 5 },
+            { id: 4, value: 19 },
+        ]
     }
 
     render() {
         return(
             <div>
-                <Counter />
-                <Counter />
-                <Counter />
+                { this.state.counters.map(counter => <Counter key={counter.id} counter={counter} onDelete={ this.handleDelete } />)}
             </div>
         );
+    }
+
+    handleDelete = (counterId) => {
+        console.log(counterId);
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters });
     }
 }
 
