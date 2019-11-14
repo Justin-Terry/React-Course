99dfee559d9ab1ef0aee8bcabd80e1801abafcd4
Justin Terry <Justin.Terry@student.csulb.edu>
2019-11-14 08:17:40 -0800

    Got through Removing the Local State

--- count-app/src/components/counter.jsx
+++ count-app/src/components/counter.jsx
@@ -1,39 +1,27 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        id: this.props.counter.id,
-        value: this.props.counter.value,
-    }
-
     render() {
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{ this.formatCount() }</span>
-                <button onClick={ this.handleIncrement } className="btn btn-success btn-sm m-2">+</button>
-                <button onClick={ this.handleDecrement } className="btn btn-warning btn-sm m-2">-</button>
-                <button onClick={ () => this.props.onDelete(this.props.counter.id) } className="btn btn-danger btn-sm m-2">X</button>
+                <button onClick={ () => this.props.onIncrement(this.props.counter) } className="btn btn-success btn-sm m-2">+</button>
+                <button onClick={ () => this.props.onDecrement(this.props.counter) } className="btn btn-warning btn-sm m-2">-</button>
+                <button onClick={ () => this.props.onDelete(this.props.counter.id) } className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
 
     // Using an arrow function here gives access to this because it inherits it
-    handleIncrement = () => {
-        this.setState({ value: this.state.value+1 });
-    }
-
-    handleDecrement = () => {
-        this.setState({ value: this.state.value-1 });
-    }
 
     getBadgeClasses() {
         var classes = "badge m-2 ";
-        classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
+        classes += this.props.counter.value === 0 ? "badge-warning" : "badge-primary";
         return classes;
     }
 
     formatCount() {
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return value === 0 ? 'Zero' : value;
     }
 }
--- count-app/src/components/counters.jsx
+++ count-app/src/components/counters.jsx
@@ -14,15 +14,47 @@ class Counters extends Component {
     render() {
         return(
             <div>
-                { this.state.counters.map(counter => <Counter key={counter.id} counter={counter} onDelete={ this.handleDelete } />)}
+                <button 
+                    className={"btn btn-primary btn-sm m-2"}
+                    onClick={this.handleReset}s
+                    >Reset</button>
+                { this.state.counters.map(counter => 
+                    <Counter key={counter.id} 
+                        counter={counter} 
+                        onIncrement={this.handleIncrement} 
+                        onDecrement={this.handleDecrement} 
+                        onDelete={ this.handleDelete } />)}
             </div>
         );
     }
 
+    handleIncrement = (counter) => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({counters});
+    }
+
+    handleDecrement = (counter) => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value--;
+        this.setState({counters});
+    }
+
     handleDelete = (counterId) => {
-        console.log(counterId);
         const counters = this.state.counters.filter(c => c.id !== counterId);
         this.setState({ counters });
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value =  0;
+            return c;
+        });
+        this.setState({counters});
     }
 }
 
